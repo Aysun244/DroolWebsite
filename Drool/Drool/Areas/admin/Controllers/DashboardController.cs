c880ceeabc96ae884a86160a7435caef30d75480
Aysun Naghizada <nagizadaysun9@gmail.com>
2025-05-01 00:20:23 +0400

    Drool

---
+++ Drool/Drool/Areas/admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drool.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
